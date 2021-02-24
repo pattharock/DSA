efa38404dd5bde8ce179248067bf1080491b31a6
pattharock <48458898+pattharock@users.noreply.github.com>
2021-02-25 07:12:30 +0800

    Update productOfArray.js

--- productOfArray.js
+++ productOfArray.js
@@ -1,14 +1,9 @@
 // accept an array and return the product of all the elements in the array.
 
 // Using pure recursion : O(n)
-function productOfArray(arr) {
-  return arr.length === 0 ? 1 : arr[0] * productOfArray(arr.slice(1));
-  // if (arr.length === 0) {
-  //   return 1;
-  // } else {
-  //   return arr[0] * productOfArray(arr.slice(1));
-  // }
-}
+// function productOfArray(arr) {
+//   return arr.length === 0 ? 1 : arr[0] * productOfArray(arr.slice(1));
+// }
 
 // Using Helper method recursion : O(n)
 function productOfArray(arr) {
