39e514c0590646dcf7c0ee0bdbd1c06e1fd0863b
pattharock <ritvik1609#gmail.com>
2021-02-25 09:29:16 +0800

    added time complexities

--- flatten.js
+++ flatten.js
@@ -1,14 +1,34 @@
 // accept an array of arrays and return a new array with all values flattened
 
+//
+// function flatten(arr) {
+//   let retArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (Array.isArray(arr[i])) {
+//       retArr = retArr.concat(flatten(arr[i]));
+//     } else {
+//       retArr.push(arr[i]);
+//     }
+//   }
+//   return retArr;
+// }
+
+// O(N) time
 function flatten(arr) {
   let retArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-      retArr = retArr.concat(flatten(arr[i]));
+
+  function helper(helperInput) {
+    if (Array.isArray(helperInput)) {
+      for (let i = 0; i < helperInput.length; i++) {
+        helper(helperInput[i]);
+      }
     } else {
-      retArr.push(arr[i]);
+      retArr.push(helperInput);
     }
   }
+
+  helper(arr);
+
   return retArr;
 }
 
